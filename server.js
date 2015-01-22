0647cafb69853753a357f57b0e7c33b0ad144bed
cianclarke <cian@cianclarke.com>
2015-01-22 14:38:18 -0500

    Add reporting support

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express'),
 logs = require('./lib/logs'),
+reports = require('./lib/reports'),
 app = express();
 
 app.get('/sys/reporter/logs', function (req, res) {
@@ -11,6 +12,18 @@ app.get('/sys/reporter/logs', function (req, res) {
   });
   
 });
+
+app.get('/sys/reporter/reports', function (req, res) {
+  reports(function(err, reportingResult){
+    if (err){
+      return res.status(500).json(err);
+    }
+    // TODO Return JSON array here instead
+    return res.end(reportingResult);
+  });
+  
+});
+
 
 app.get('/sys/reporter/ping', function(req, res){
   return res.json({ok : true});
